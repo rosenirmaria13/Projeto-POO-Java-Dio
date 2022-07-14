d8995e1d8bfde7874d39a0087a73c32203542d7b
Rosenir Silva <rosenirmaria13@gmail.com>
2022-07-14 20:38:43 -0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.projeto.dominio.Curso;
-import br.com.dio.projeto.dominio.Mentoria;
+import br.com.dio.projeto.dominio.*;
 
 import java.time.LocalDate;
 
@@ -15,13 +14,47 @@ public class Main {
         curso2.setDescricao("descrição curso javaScript");
         curso2.setCargaHoraria(4);
 
-        Mentoria mentoria = new Mentoria();
+        Conteudo conteudo = new Curso();
+
+        Mentoria mentoria = new Mentoria() {
+        };
         mentoria.setTitulo("mentoria de Java");
         mentoria.setDescricao("descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRosenir = new Dev();
+        devRosenir.setNome("Rosenir");
+        devRosenir.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Rosenir" + devRosenir.getConteudosInscritos());
+        devRosenir.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Rosenir" + devRosenir.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Rosenir" + devRosenir.getConteudosConcluidos());
+        System.out.println("XP: " + devRosenir.calcularTotalXp());
+
+        System.out.println("------");
+
+        Dev devIago = new Dev();
+        devIago.setNome("Iago");
+        devIago.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Iago" + devIago.getConteudosInscritos());
+        devIago.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Iago" + devIago.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Iago" + devIago.getConteudosConcluidos());
+        System.out.println("XP: " + devIago.calcularTotalXp());
+
     }
 }
+
--- src/br/com/dio/projeto/dominio/Curso.java
+++ src/br/com/dio/projeto/dominio/Curso.java
@@ -1,29 +1,15 @@
 package br.com.dio.projeto.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
-    private int cargaHoraria;
+public class Curso extends Conteudo{
+       private int cargaHoraria;
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
+    }
 
     public Curso() {
     }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -35,8 +21,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
