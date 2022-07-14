aa912ae7b67a59e3f401ffcf0279853858ab2275
Rosenir Silva <rosenirmaria13@gmail.com>
2022-07-14 18:20:50 -0300

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import br.com.dio.projeto.dominio.Curso;
+import br.com.dio.projeto.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso java");
+        curso2.setDescricao("descrição curso javaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
